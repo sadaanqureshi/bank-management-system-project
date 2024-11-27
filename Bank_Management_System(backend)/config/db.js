272bbfa56296425a3f1b9587be9b7174b326c7a6
Muhammad Sadaan Qureshi <126042218+sadaanqureshi1@users.noreply.github.com>
2024-11-27 21:51:59 +0500

    Update db.js

--- Bank_Management_System(backend)/config/db.js
+++ Bank_Management_System(backend)/config/db.js
@@ -3,8 +3,8 @@ const mysql=require('mysql2/promise');
 const mySqlPool=mysql.createPool({
   host: 'localhost',
     user: 'root',      
-    password: 'fast',  
-    database: 'bank-management' ,
+    password: 'fast123',  
+    database: 'bank-management-system' ,
     
   });
   module.exports=mySqlPool;
